499cfbf298472a9c8a67c35afcd3846a25c2353b
Chantal <chantalwilkister@gmail.com>
2023-02-24 00:50:59 -0800

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ * mul - multiplies integers
+ * @a: a(variable)
+ * @b: b(variable)
+ * Return: 0(success)
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+
+	Return(mul);
+}
