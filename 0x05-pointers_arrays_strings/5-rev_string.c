351821eb31fa590543106d3f95ecbda3e80d74dd
Chantal <chantalwilkister@gmail.com>
2023-02-27 10:32:32 -0800

    rev

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,18 +10,18 @@ void rev_string(char *s)
 	char first, last;
 
 	i = 0;
-	while (s[i] != '\0\)
-			{
-			i++;
-			}
-			max = i - 1;
-			half = max / 2;
-			while (half >= 0)
-			{
-			first = s[max - half];
-			last s[half];
-			s[half] = first;
-			s[max - half] = last;
-			half --;
-			}
-			}
+	while (s[i] != '\0\')
+	{
+		i++;
+	}
+	max = i - 1;
+	half = max / 2;
+	while (half >= 0)
+	{
+		first = s[max - half];
+		last s[half];
+		s[half] = first;
+		s[max - half] = last;
+		half--;
+	}
+}
