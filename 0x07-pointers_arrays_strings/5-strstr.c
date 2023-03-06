cb6e394491955902a608e48e4c2c7fdfb32879e2
Chantal <chantalwilkister@gmail.com>
2023-03-06 12:55:54 -0800

    5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,30 @@
+#include"main.h"
+/**
+ * _strstr - locates a substring
+ * @haystack: haystack character
+ * @needle: needle character
+ * Return: s/NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *result = haystack, *needle = needle;
+
+	while (*haystack)
+	{
+		while (*needle)
+		{
+			if (*haystack++ != *needle++)
+			{
+				break;
+			}
+		}
+		if (!*needle)
+		{
+			return (result);
+		}
+		needle = fneedle;
+		result++;
+		haystack = result;
+	}
+	return (0);
+}
